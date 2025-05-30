16be4d07ebdcd96eaa8d98645b9fa4abce34ad84
alexiaboboc <126000922+alexiaboboc@users.noreply.github.com>
2025-05-30 13:45:03 +0300

    domain + application

---
+++ DocumentAnalysis.Domain/Entities/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentAnalysis.Domain.Entities
+{
+    public class Document
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FileName { get; set; } = string.Empty;
+        public string FileType { get; set; } = string.Empty; 
+        public byte[] Content { get; set; } = Array.Empty<byte>();
+        public string? ExtractedText { get; set; }
+    }
+}
